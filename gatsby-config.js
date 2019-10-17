aeba0b9baf27b00b2eb08b430e65379dd93b35db
amatracker <56606520+amatracker@users.noreply.github.com>
2019-10-17 22:39:17 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     name: ``,
-    title: ``,
+    title: `AmaTracker`,
     date: ``,
   },
   plugins: [
@@ -34,16 +34,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
   ],
 };
